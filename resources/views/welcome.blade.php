6db836149d8c2d967f01763dce0b44a7c20c077b
Quinn Zipse <qzipse@outlook.com>
2018-07-19 13:45:15 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Quinn Zipse</title>
+
+        <link href="{{"css/app.css"}}" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div class="container">
+            <h1>Welcome to my website!</h1>
+            <div class="divider"></div>
+            <p>Made by: Quinn Zipse <small>(I mean duh, look at the domain name)</small></p>
+        </div>
+    </body>
+</html>
