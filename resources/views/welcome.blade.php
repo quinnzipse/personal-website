4326e944caece1f4db2a05fa64ef1e02527fdaba
Quinn Zipse <qzipse@outlook.com>
2018-07-19 22:11:01 -0500

    Made the website a lot better

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,22 +15,41 @@
     </head>
     <body>
     <nav>
-        <div class="nav-wrapper">
-            <a href="" class="brand-logo"><img src=""></a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="">About Me</a></li>
-                <li><a href="">Login</a></li>
-            </ul>
-        </div>
+        <ul class="left">
+            <li><a href="#">quinnzipse.me<small class="hide-on-large-only" style="font-size: 7px"> Beta</small></a></li>
+        </ul>
+        <ul class="right">
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
     </nav>
-    <main>
-        <div class="main-contain"></div>
-        <footer class="page-footer">
+    <main class="blue">
+        <div class="container" style="margin-top: 18%">
+            <br>
+        </div>
+        <div class="container" style="height: 50%">
+            <h2 class="white-text hide-on-med-and-down">Hello, I am Quinn Zipse</h2>
+            <h5 class="white-text hide-on-large-only">Hello, I am Quinn Zipse</h5>
+            <h5 class="white-text hide-on-med-and-down"style="padding-left: 3px">Programming is what I do.</h5>
+            <h6 class="white-text hide-on-large-only" style="padding-left: 3px">Programming is what I do.</h6>
+            <br>
+            <br>
+            <button class="btn-flat white-text z-depth-4" style="padding-left: 5px">Learn More >></button>
+        </div>
+        <div class="container hide-on-med-and-up" style="margin-top: 98%">
+            <small class="white-text" style="font-size: 55%">This site is not optimized for mobile devices. For full experience, please view on a desktop.</small>
+        </div>
+    </main>
+        <footer class="page-footer blue darken-1">
             <div class="footer-copyright">
-                <p class="white-text" style="margin-left: 10px">  © 2018 Quinn Zipse</p>
+                    <div class="footer-container left">
+                        © 2018 Quinn Zipse
+                    </div>
+                <div class="footer-container right" style="margin-right: 1%">
+                    <div class="col m3"><i></i>Github</div>
+                </div>
             </div>
         </footer>
-    </main>
     </body>
 
 </html>
