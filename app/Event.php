7441131ce2e3933f159f816c72a02a18a8937c45
Quinn Zipse <qzipse@outlook.com>
2018-07-29 17:28:50 -0500

    I'm not actually sure what I did

--- app/Event.php
+++ app/Event.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = [
-        'name', 'time', 'date', 'type', 'prioritys'
+        'name', 'time', 'date', 'type'
     ];
 
     protected $hidden = [
