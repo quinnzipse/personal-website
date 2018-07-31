8da343967f9ae585db47e2335b15f3ddbc199325
Quinn Zipse <qzipse@outlook.com>
2018-07-30 22:19:21 -0500

    fixing bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="{{'img/favicon.ico'}}">
+    {{--<link rel="icon" href="{{'img/favicon.ico'}}">--}}
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
