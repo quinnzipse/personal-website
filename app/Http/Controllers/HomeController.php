e64d3ff519690838fea35ab477bbde52cadcbd87
Quinn Zipse <qzipse@outlook.com>
2018-08-28 18:21:22 -0500

    spotify stuff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\SpotifySettings;
+use App\User;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,4 +27,37 @@ class HomeController extends Controller
     {
         return view('dashboard/home');
     }
+
+    public function story(){
+        $currentUser = User::where('spotUsername', '=', 'qzipse-us')->get()[0];
+        $spotSettings = SpotifySettings::where('spotUsername', '=', 'qzipse-us')->get()[0];
+        $client = new Client();
+
+        //init variables
+        $loggedInSpotify = false;
+        $image_url = null;
+        $body = null;
+
+        //check if user is logged into spotify & if they want the public to see what they are listening to
+        if($currentUser->authToken != '' && $spotSettings->plisten){
+
+            //If user is logged in, get his current status from spotify
+            $loggedInSpotify = true;
+            $res = $client->request('GET', 'https://api.spotify.com/v1/me/player/currently-playing',
+                ["headers" => [
+                    "Authorization" => 'Bearer ' . $currentUser->authToken
+                ]]
+            );
+
+            //Logged in but not playing anything
+            if($res->getBody() == '') return view('story', ['loggedInSpotify' => $loggedInSpotify, 'image_url' => null, 'song_details' => null]);
+
+            //parse the spotify data
+            $body = json_decode($res->getBody());
+            $image_url = $body->item->album->images[1]->url;
+        }
+        //TODO: add a way to set hue logged in to true once the feature is up
+        return view('story', ['loggedInSpotify' => $loggedInSpotify, 'image_url' => $image_url, 'song_details' => $body]);
+    }
+
 }
--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -1,7 +1,85 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: qzips
- * Date: 8/28/2018
- * Time: 10:05 AM
- */
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keyboards"
+          content="Quinn, quinn, zipse,  Zipse, Quinn Zipse, quinn zipse, quinnzipse, QuinnZipse, SlippedMars9866">
+    <meta name="description" content="Just a look into a programmers life online.">
+
+    <title>Quinn Zipse</title>
+
+    <link href="{{"css/app.css"}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
+          integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <style type="text/css">
+        body {
+            position: relative;
+        }
+    </style>
+</head>
+<body data-spy="scroll" data-target="#navbar" data-offset="10">
+<nav class="navbar navbar-expand-lg navbar-light bg-primary" id="navbar" style="position:fixed;top:0;left:0;right:0;">
+    <a href="#main" class="navbar-brand text-white">quinnzipse.me</a>
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target=".navbar-collapse"
+            aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars text-white" style="font-size: .9em"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a href="#aboutme" class="nav-link text-white">About Me</a></li>
+            <li class="nav-item"><a href="{{"login"}}" class="nav-link text-white">Login</a></li>
+        </ul>
+    </div>
+</nav>
+<br>
+@if($loggedInSpotify) <!-- This will hit when ONLY Spotify is connected -->
+@if($song_details != null)
+    @php
+        $song_name = $song_details->item->name;
+        $artist = $song_details->item->artists[0]->name;
+        $timeout = $song_details->item->duration_ms - $song_details->progress_ms; //Took out an offset because it was no longer needed
+        $playing = $song_details->is_playing;
+    @endphp
+    <script type="text/javascript">
+        setTimeout(function () {
+            location.reload();
+        }, {{$timeout}});
+    </script>
+    <div class="container mt-5 row">
+    <div class="offset-3 col-lg-5 col-7 col-sm-7">
+            <div class="col-12">
+                        <div class="container">
+                            <div class=" d-flex justify-content-center">
+                                <img src="{{$image_url}}" style="height: 75%; width: 75%;">
+                            </div>
+                        </div>
+        </div>
+    </div>
+        <div class="col-lg-4 col-5">
+            <div class="row mt-3">
+            @if(strlen($song_name) > 17)
+                <h4 class="font-weight-normal truncate">{{$song_name}}</h4>
+            @else
+                <h2 class="font-weight-normal">{{$song_name}}</h2>
+            @endif
+            </div>
+                <div class="row">
+                    <h5 class="font-weight-light float-left">{{$artist}}</h5>
+                </div>
+        </div>
+    </div>
+@else
+    <div class="container h-100">
+        <div class="justify-content-center d-flex h-100">
+            <div class="align-items-center d-flex">
+                <h3 class="text-faint font-weight-light">Nothing is playing right now.</h3>
+            </div>
+        </div>
+    </div>
+@endif
+@endif
+</body>
+</html>
