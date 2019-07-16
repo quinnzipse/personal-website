854c947196b7bd4e8c1210728b4a3d4dae8a8188
Quinn Zipse <qzipse@outlook.com>
2019-07-16 08:12:40 -0500

    link to twitter was incorrect. Fixed.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,6 @@
     <span style="margin-left: 1%">© 2018 Quinn Zipse</span>
     <a href="https://www.facebook.com/quinnzipse" class="float-right mr-3 ml-4"><i
                 class="fab fa-facebook text-white"></i> Facebook </a>
-    <a href="https://www.twitter.com/quinn_zipse" class="float-right"><i
+    <a href="https://www.twitter.com/quinnzipse" class="float-right"><i
                 class="fab fa-twitter text-white"></i> Twitter</a>
 </footer>
