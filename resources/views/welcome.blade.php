672c393908a846dbb6c75545464885c3c5b68b2c
Quinn Zipse <qzipse@outlook.com>
2020-07-25 16:02:33 -0500

    worked on responsive design, looks pretty good.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,6 +31,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
 
 </head>
 <body>
@@ -49,206 +50,40 @@
                 </ul>
             </div>
         </div>
-        <div id="image_container" class="">
-            <div id="inner_container">
-                <img src="{{asset('/img/quinn_color.jpg')}}" alt="a picture of me :)">
-            </div>
-        </div>
     </div>
+    <hr id="the_divide">
     <div id="about-panel">
         <div id="about_panel_container">
             <div id="about">
-                <p><span>Web Developer by day, Chicken Tender by night.</span> Born and raised in Southeast Minnesota.
+                <p><span>Web Developer by day, Chicken Tender by night.</span><br> Born and raised in Southeast
+                    Minnesota.
                     Full-time student at The University of Wisconsin - La Crosse. Majoring in Computer Science,
-                    graduating Fall of 2022.
+                    Class of 2023.
                     Tesla and Google fanboy. Currently an intern at AgVantage Software, Inc. where I develop web apps
                     for agricultural co-ops around the nation.</p>
             </div>
-            <div class="d-inline-flex" style="width: 49%">
+            <div id="other_stuff">
+                <div id="image_container" class="">
+                    <img src="{{asset('/img/quinn_color.jpg')}}" alt="a picture of me :)">
+                </div>
                 <div id="connections">
                     <div id="conn_text_container">
                         <h4>Get in touch!</h4>
                     </div>
-                    <div id="connect_container">
-                        <div id="brand_container">
-                            <a href="https://instagram.com/quinn.zipse"><i class="fab fa-instagram"></i></a>
-                            <a href="https://facebook.com/quinnzipse"><i class="fab fa-facebook-f"></i></a>
-                            <a href="https://linkedin.com/in/quinnzipse"><i class="fab fa-linkedin-in"></i></a>
-                            <a href="https://github.com/quinnzipse"><i class="fab fa-github"></i></a>
-                            <a href="https://twitter.com/quinnzipse"><i class="fab fa-twitter"></i></a>
-                        </div>
+                    <div id="brand_container">
+                        <a href="https://instagram.com/quinn.zipse"><i class="fab fa-instagram"></i></a>
+                        <a href="https://facebook.com/quinnzipse"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://linkedin.com/in/quinnzipse"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://github.com/quinnzipse"><i class="fab fa-github"></i></a>
+                        <a href="https://twitter.com/quinnzipse"><i class="fab fa-twitter"></i></a>
+                        <a href="mailto:heyitsme@quinnzipse.dev"><i class="far fa-envelope"></i></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    {{--    <div id="contact-panel">--}}
+    {{--    </div>--}}
 </main>
-<style type="text/css">
-
-    #main_content {
-        display: flex;
-        width: 100%;
-        height: 55vh;
-
-        background: rgb(40, 80, 46) linear-gradient(360deg, rgba(40, 80, 46, 1) 10%, rgba(27, 47, 51, 1) 91%);
-    }
-
-    #inner_container {
-        border: 3px solid white;
-        border-radius: 50%;
-    }
-
-    #image_container {
-        width: 23%;
-        height: fit-content;
-        margin: auto auto -3px auto;
-        /*border: 5px solid rgba(60, 60, 60, .1);*/
-        border-radius: 50%;
-        /*box-shadow: 3px 10px rgba(0, 0, 0, .1);*/
-    }
-
-    #image_container img {
-        width: 100%;
-        margin: auto;
-        border-radius: 50%;
-    }
-
-    body {
-        font-family: 'Major Mono Display', monospace;
-    }
-
-    #text_container {
-        font-family: 'Major Mono Display', monospace;
-        margin: auto 0 4% auto;
-        width: fit-content;
-        font-size: 2.5rem;
-        line-height: 2.6rem;
-    }
-
-    #text_container h1 {
-        font-size: 4.5rem;
-        line-height: 4.5rem;
-    }
-
-    #text_container h2 {
-        font-size: 1.8rem;
-        margin-left: 4px;
-        line-height: 1.8rem;
-    }
-
-    .description-container {
-        overflow: hidden;
-        height: 3rem;
-        margin-top: 45px;
-    }
-
-    .description-list {
-        padding-left: 4px;
-        margin-top: 0;
-        text-align: left;
-        list-style: none;
-        animation: change 12s infinite;
-    }
-
-    .description-item {
-        line-height: 2.6rem;
-        margin: 0;
-    }
-
-    @-webkit-keyframes change {
-        0%, 16%, 100% {
-            transform: translateY(0);
-        }
-        20%, 36% {
-            transform: translateY(-20%);
-        }
-        40%, 56% {
-            transform: translateY(-40%);
-        }
-        60%, 76% {
-            transform: translateY(-60%);
-        }
-        80%, 96% {
-            transform: translateY(-80%);
-        }
-
-    }
-</style>
-<style type="text/css">
-    #about-panel {
-        margin-top: 3px;
-        height: calc(45vh - 3px);
-        /*background: linear-gradient(360deg, rgba(27,47,51,1) 5%, rgba(40,80,46,1) 36%,  91%);*/
-        background-color: rgba(85, 116, 60, 1);
-        display: flex;
-    }
-
-    #about_panel_container {
-        margin: auto 6%;
-        display: flex;
-    }
-
-    #about {
-        display: inline-flex;
-        color: ghostwhite;
-        font-family: 'Montserrat', sans-serif;
-        width: 50%;
-        text-align: right;
-        line-height: 2rem;
-        padding: 2%;
-        border-right: 1px solid floralwhite;
-    }
-
-    #about p {
-        margin-top: 1%;
-        font-size: 1.1rem;
-    }
-
-    #about span {
-        color: #EF8275;
-        font-size: 1.5rem;
-    }
-
-    #connections {
-        display: inline-grid;
-        width: 100%;
-        margin: auto 15%;
-        height: 75%;
-        padding: 4% 0;
-        color: floralwhite;
-        /*background: linear-gradient(0deg, rgba(91, 133, 170, .7), rgba(91, 133, 170, .3));*/
-        background: linear-gradient(360deg, rgba(40, 80, 46, .2) 10%, rgba(27, 47, 51, .25) 91%);
-        /*border-bottom: 1px solid rgba(239, 130, 117, .4);*/
-        border-radius: 15px;
-    }
-    #conn_text_container {
-        margin: 0 auto;
-    }
-
-    #conn_text_container h4 {
-        font-family: 'Montserrat', sans-serif;
-        font-size: 2.3rem;
-        color: #EF8275;
-    }
-
-    #brand_container {
-        display: flex;
-        width: 55%;
-        margin: auto;
-        padding-top: 4%;
-    }
-
-    #brand_container a {
-        margin: auto;
-        color: floralwhite;
-        font-size: 1.5rem;
-    }
-
-    #brand_container a:hover {
-        color: lightgray;
-    }
-
-
-</style>
 </body>
 </html>
